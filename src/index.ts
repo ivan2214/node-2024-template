2a7f1579da381d03336aaf3b5c9dae2ac4c13434
Ivan Bongiovanni <bongiovanniivan12gmail.com>
2024-08-12 22:02:24 -0300

    feat: add nodemon

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { add } from "./utils";
+
+console.log(add(0, 1));
+
+console.log("Debug", process.env.APP_DEBUG);
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,9 @@
+import { debug } from "console";
+import config from "./config";
+
 export function add(a: number, b: number): number {
+  if (config.debug) {
+    debug(`Calling add function with args ${a} and ${b}`);
+  }
   return a + b;
 }
